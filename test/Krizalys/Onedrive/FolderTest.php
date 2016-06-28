83ef4edd896b3688eb3098a601453cb8ed4ece2b
Christophe Vidal <kriss@krizalys.com>
2016-06-28 21:13:16 +0700

    Adjusted coding style

--- test/Krizalys/Onedrive/FolderTest.php
+++ test/Krizalys/Onedrive/FolderTest.php
@@ -1,68 +1,69 @@
 <?php
 
-namespace Test\Krizalys\Onedrive
+namespace Test\Krizalys\Onedrive;
+
+use Krizalys\Onedrive\Folder;
+
+class FolderTest extends \PHPUnit_Framework_TestCase
 {
-    use Krizalys\Onedrive\Folder;
-
-    class FolderTest extends \PHPUnit_Framework_TestCase
+    public function testFetchDescendantObjects()
     {
-        public function testFetchDescendantObjects()
-        {
-            $file1 = $this->getFileMock('file1');
-            $file2 = $this->getFileMock('file2');
-            $file3 = $this->getFileMock('file3');
-            $file4 = $this->getFileMock('file4');
+        $file1 = $this->getFileMock('file1');
+        $file2 = $this->getFileMock('file2');
+        $file3 = $this->getFileMock('file3');
+        $file4 = $this->getFileMock('file4');
 
-            $folderMock = $this->getFolderMock(
-                array(
-                    $file1,
-                    $this->getFolderMock(
-                        array(
-                            $file2,
-                            $file3,
-                        )
-                    ),
-                    $file4,
-                )
-            );
+        $folderMock = $this->getFolderMock(
+            array(
+                $file1,
+                $this->getFolderMock(
+                    array(
+                        $file2,
+                        $file3,
+                    )
+                ),
+                $file4,
+            )
+        );
 
-            $expected = array($file2, $file3, $file1, $file4);
-            $actual   = $folderMock->fetchDescendantObjects();
+        $expected = array($file2, $file3, $file1, $file4);
+        $actual   = $folderMock->fetchDescendantObjects();
+        $this->assertEquals($expected, $actual);
+    }
 
-            $this->assertEquals($expected, $actual);
-        }
+    /**
+     * @param object[] $childObjects
+     *
+     * @return \PHPUnit_Framework_MockObject_MockObject|Folder
+     */
+    protected function getFolderMock($childObjects)
+    {
+        $mock = $this
+            ->getMockBuilder('Krizalys\Onedrive\Folder')
+            ->disableOriginalConstructor()
+            ->setMethods(array('fetchChildObjects'))
+            ->getMock();
 
-        /**
-         * @param object[] $childObjects
-         *
-         * @return \PHPUnit_Framework_MockObject_MockObject|Folder
-         */
-        protected function getFolderMock($childObjects)
-        {
-            $mock = $this->getMockBuilder('Krizalys\Onedrive\Folder')
-                ->disableOriginalConstructor()
-                ->setMethods(array('fetchChildObjects'))
-                ->getMock();
+        $mock
+            ->method('fetchChildObjects')
+            ->willReturn($childObjects);
 
-            $mock->method('fetchChildObjects')
-                ->willReturn($childObjects);
+        return $mock;
+    }
 
-            return $mock;
-        }
+    /**
+     * @param mixed $fileId
+     *
+     * @return \PHPUnit_Framework_MockObject_MockObject|File
+     */
+    protected function getFileMock($fileId)
+    {
+        $mock = $this
+            ->getMockBuilder('Krizalys\Onedrive\File')
+            ->disableOriginalConstructor()
+            ->getMock();
 
-        /**
-         * @param mixed $fileId
-         *
-         * @return \PHPUnit_Framework_MockObject_MockObject|File
-         */
-        protected function getFileMock($fileId)
-        {
-            $mock = $this->getMockBuilder('Krizalys\Onedrive\File')
-                ->disableOriginalConstructor()
-                ->getMock();
-
-            $mock->id = $fileId;
-            return $mock;
-        }
+        $mock->id = $fileId;
+        return $mock;
     }
 }
