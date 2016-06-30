494a1a84b0d2b26a6074abdc3f73c22ab7b92106
Christophe Vidal <kriss@krizalys.com>
2016-06-30 22:15:51 +0700

    Added test for Client::fetchShared()

--- test/Krizalys/Onedrive/ClientTest.php
+++ test/Krizalys/Onedrive/ClientTest.php
@@ -896,6 +896,28 @@ class ClientTest extends \PHPUnit_Framework_TestCase
         $actual = $arguments['value'];
         $this->assertEquals('https://apis.live.net/v5.0/me/skydrive/recent_docs?access_token=OlD%2FAcCeSs%2BToKeN', $actual);
     }
+
+    public function testFetchSharedUrl()
+    {
+        $this->mockCurlSetoptArray();
+
+        $arguments = array();
+        $this->mockCurlSetopt(true, $arguments);
+
+        $this->mockCurlExec(json_encode((object) array(
+            'id'   => 'folder.ffffffffffffffff.FFFFFFFFFFFFFFFF!123',
+            'type' => 'folder',
+        )));
+
+        $this->mockCurlGetinfo();
+
+        $this
+            ->client
+            ->fetchShared();
+
+        $actual = $arguments['value'];
+        $this->assertEquals('https://apis.live.net/v5.0/me/skydrive/shared?access_token=OlD%2FAcCeSs%2BToKeN', $actual);
+    }
 }
 
 /**
