edf40685d3cd55dee6504d5565986cd221537d27
Christophe Vidal <kriss@krizalys.com>
2016-07-01 18:55:31 +0700

    Refactoring

--- test/Krizalys/Onedrive/ObjectTest.php
+++ test/Krizalys/Onedrive/ObjectTest.php
@@ -11,6 +11,18 @@ class TestObject extends Object
 
 class ObjectTest extends \PHPUnit_Framework_TestCase
 {
+    private $object;
+
+    private $client;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $client       = $this->mockClient();
+        $this->object = new TestObject($client, 'file.ffffffffffffffff.FFFFFFFFFFFFFFFF!123');
+        $this->client = $client;
+    }
+
     private function mockPayloadFolder(array $values = array())
     {
         return (object) array_merge(array(
@@ -39,21 +51,33 @@ class ObjectTest extends \PHPUnit_Framework_TestCase
 
     public function testIsFolderShouldReturnFalse()
     {
-        $client = $this->mockClient();
-        $object = new TestObject($client, 'file.ffffffffffffffff.FFFFFFFFFFFFFFFF!123');
-        $actual = $object->isFolder();
+        $actual = $this
+            ->object
+            ->isFolder();
+
         $this->assertFalse($actual);
     }
 
     public function testGetIdShouldReturnExpectedValue()
     {
-        $client = $this->mockClient();
-        $object = new TestObject($client, 'file.ffffffffffffffff.FFFFFFFFFFFFFFFF!123');
-        $actual = $object->getId();
+        $actual = $this
+            ->object
+            ->getId();
+
         $this->assertEquals('file.ffffffffffffffff.FFFFFFFFFFFFFFFF!123', $actual);
     }
 
-    public function testFetchPropertiesShouldSetParentId()
+    public function testGetParentIdWithParentIdShouldReturnExpectedValue()
+    {
+        $object = new TestObject($this->client, 'file.ffffffffffffffff.FFFFFFFFFFFFFFFF!456', array(
+            'parent_id' => 'folder.ffffffffffffffff.FFFFFFFFFFFFFFFF!123',
+        ));
+
+        $actual = $object->getParentId();
+        $this->assertEquals('folder.ffffffffffffffff.FFFFFFFFFFFFFFFF!123', $actual);
+    }
+
+    public function testGetParentIdWithoutParentIdShouldReturnExpectedValue()
     {
         $payload = $this->mockPayloadFolder(array(
             'parent_id' => 'folder.ffffffffffffffff.FFFFFFFFFFFFFFFF!123',
@@ -65,13 +89,22 @@ class ObjectTest extends \PHPUnit_Framework_TestCase
             },
         ));
 
-        $object = new TestObject($client, 'file.ffffffffffffffff.FFFFFFFFFFFFFFFF!456');
-        $object->fetchProperties();
+        $object = new TestObject($client, 'file.ffffffffffffffff.FFFFFFFFFFFFFFFF!123');
         $actual = $object->getParentId();
         $this->assertEquals('folder.ffffffffffffffff.FFFFFFFFFFFFFFFF!123', $actual);
     }
 
-    public function testFetchPropertiesShouldSetName()
+    public function testGetNameWithNameShouldReturnExpectedValue()
+    {
+        $object = new TestObject($this->client, 'file.ffffffffffffffff.FFFFFFFFFFFFFFFF!123', array(
+            'name' => 'test-object',
+        ));
+
+        $actual = $object->getName();
+        $this->assertEquals('test-object', $actual);
+    }
+
+    public function testGetNameWithoutNameShouldReturnExpectedValue()
     {
         $payload = $this->mockPayloadFolder(array(
             'name' => 'test-object',
@@ -83,13 +116,22 @@ class ObjectTest extends \PHPUnit_Framework_TestCase
             },
         ));
 
-        $object = new TestObject($client, 'file.ffffffffffffffff.FFFFFFFFFFFFFFFF!456');
-        $object->fetchProperties();
+        $object = new TestObject($client, 'file.ffffffffffffffff.FFFFFFFFFFFFFFFF!123');
         $actual = $object->getName();
         $this->assertEquals('test-object', $actual);
     }
 
-    public function testFetchPropertiesShouldSetDescription()
+    public function testGetDescriptionWithDescriptionShouldReturnExpectedValue()
+    {
+        $object = new TestObject($this->client, 'file.ffffffffffffffff.FFFFFFFFFFFFFFFF!123', array(
+            'description' => 'Some test description',
+        ));
+
+        $actual = $object->getDescription();
+        $this->assertEquals('Some test description', $actual);
+    }
+
+    public function testGetDescriptionWithoutDescriptionShouldReturnExpectedValue()
     {
         $payload = $this->mockPayloadFolder(array(
             'description' => 'Some test description',
@@ -102,12 +144,21 @@ class ObjectTest extends \PHPUnit_Framework_TestCase
         ));
 
         $object = new TestObject($client, 'file.ffffffffffffffff.FFFFFFFFFFFFFFFF!456');
-        $object->fetchProperties();
         $actual = $object->getDescription();
         $this->assertEquals('Some test description', $actual);
     }
 
-    public function testFetchPropertiesShouldSetSize()
+    public function testGetSizeWithSizeShouldReturnExpectedValue()
+    {
+        $object = new TestObject($this->client, 'file.ffffffffffffffff.FFFFFFFFFFFFFFFF!123', array(
+            'size' => 123,
+        ));
+
+        $actual = $object->getSize();
+        $this->assertEquals(123, $actual);
+    }
+
+    public function testGetSizeWithoutSizeShouldReturnExpectedValue()
     {
         $payload = $this->mockPayloadFolder(array(
             'size' => 123,
@@ -119,13 +170,22 @@ class ObjectTest extends \PHPUnit_Framework_TestCase
             },
         ));
 
-        $object = new TestObject($client, 'file.ffffffffffffffff.FFFFFFFFFFFFFFFF!456');
-        $object->fetchProperties();
+        $object = new TestObject($client, 'file.ffffffffffffffff.FFFFFFFFFFFFFFFF!123');
         $actual = $object->getSize();
         $this->assertEquals(123, $actual);
     }
 
-    public function testFetchPropertiesShouldSetSource()
+    public function testGetSourceWithSourceShouldReturnExpectedValue()
+    {
+        $object = new TestObject($this->client, 'file.ffffffffffffffff.FFFFFFFFFFFFFFFF!123', array(
+            'source' => 'http://te.st/123/source',
+        ));
+
+        $actual = $object->getSource();
+        $this->assertEquals('http://te.st/123/source', $actual);
+    }
+
+    public function testGetSourceWithoutSourceShouldReturnExpectedValue()
     {
         $payload = $this->mockPayloadFolder(array(
             'source' => 'http://te.st/123/source',
@@ -137,13 +197,22 @@ class ObjectTest extends \PHPUnit_Framework_TestCase
             },
         ));
 
-        $object = new TestObject($client, 'file.ffffffffffffffff.FFFFFFFFFFFFFFFF!456');
-        $object->fetchProperties();
+        $object = new TestObject($client, 'file.ffffffffffffffff.FFFFFFFFFFFFFFFF!123');
         $actual = $object->getSource();
         $this->assertEquals('http://te.st/123/source', $actual);
     }
 
-    public function testFetchPropertiesShouldSetCreatedTime()
+    public function testGetCreatedTimeWithCreatedTimeShouldReturnExpectedValue()
+    {
+        $object = new TestObject($this->client, 'file.ffffffffffffffff.FFFFFFFFFFFFFFFF!123', array(
+            'created_time' => '1999-12-31T23:59:59+0000',
+        ));
+
+        $actual = $object->getCreatedTime();
+        $this->assertEquals(strtotime('1999-12-31T23:59:59+0000'), $actual);
+    }
+
+    public function testGetCreatedTimeWithoutCreatedTimeShouldReturnExpectedValue()
     {
         $payload = $this->mockPayloadFolder(array(
             'created_time' => '1999-12-31T23:59:59+0000',
@@ -155,13 +224,22 @@ class ObjectTest extends \PHPUnit_Framework_TestCase
             },
         ));
 
-        $object = new TestObject($client, 'file.ffffffffffffffff.FFFFFFFFFFFFFFFF!456');
-        $object->fetchProperties();
+        $object = new TestObject($client, 'file.ffffffffffffffff.FFFFFFFFFFFFFFFF!123');
         $actual = $object->getCreatedTime();
         $this->assertEquals(strtotime('1999-12-31T23:59:59+0000'), $actual);
     }
 
-    public function testFetchPropertiesShouldSetUpdatedTime()
+    public function testGetUpdatedTimeWithUpdatedTimeShouldReturnExpectedValue()
+    {
+        $object = new TestObject($this->client, 'file.ffffffffffffffff.FFFFFFFFFFFFFFFF!123', array(
+            'updated_time' => '1999-12-31T23:59:59+0000',
+        ));
+
+        $actual = $object->getUpdatedTime();
+        $this->assertEquals(strtotime('1999-12-31T23:59:59+0000'), $actual);
+    }
+
+    public function testGetUpdatedTimeWithoutUpdatedTimeShouldReturnExpectedValue()
     {
         $payload = $this->mockPayloadFolder(array(
             'updated_time' => '1999-12-31T23:59:59+0000',
@@ -173,8 +251,7 @@ class ObjectTest extends \PHPUnit_Framework_TestCase
             },
         ));
 
-        $object = new TestObject($client, 'file.ffffffffffffffff.FFFFFFFFFFFFFFFF!456');
-        $object->fetchProperties();
+        $object = new TestObject($client, 'file.ffffffffffffffff.FFFFFFFFFFFFFFFF!123');
         $actual = $object->getUpdatedTime();
         $this->assertEquals(strtotime('1999-12-31T23:59:59+0000'), $actual);
     }
