84871cae6b3f376a0d5eeab9d4308be5b7fbe841
Christophe Vidal <kriss@krizalys.com>
2016-07-10 16:26:06 +0700

    Added test suite bootstrap

---
+++ test/bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once sprintf('%s/vendor/autoload.php', dirname(__DIR__));
+require_once sprintf('%s/Krizalys/Onedrive/functions.php', __DIR__);
+
+spl_autoload_register(function ($class) {
+    if (0 === substr_compare($class, 'Test\\Mock\\', 0, 10)) {
+        $suffix = substr($class, 10);
+        $path   = str_replace('\\', DIRECTORY_SEPARATOR, $suffix) . '.php';
+        require_once sprintf('%s/Mock/%s', __DIR__, $path);
+    }
+});
