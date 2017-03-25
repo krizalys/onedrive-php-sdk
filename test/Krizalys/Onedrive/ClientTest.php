d15a4f064f670f55068d1d941711054cccdae582
Christophe Vidal <kriss@krizalys.com>
2017-03-25 19:15:34 +0700

    Fixed variable name

--- test/Krizalys/Onedrive/ClientTest.php
+++ test/Krizalys/Onedrive/ClientTest.php
@@ -45,6 +45,8 @@ class ClientTest extends MockeryTestCase
                         'data'     => self::mockTokenData(),
                     ),
                 ),
+                'ssl_verify' => true,
+                'ssl_capath' => '/path/to/ca',
             ),
             $options
         );
