7f6f27978114b7b1b970e8ffa2124f05a70cf3e1
Christophe Vidal <kriss@krizalys.com>
2017-03-25 22:01:34 +0700

    Moved integration test suite to functional test suite

---
+++ test/functional/ClientTest.php
@@ -0,0 +1,200 @@
+<?php
+
+namespace Test\Integration\Krizalys;
+
+use Krizalys\Onedrive\Client;
+
+/**
+ * @group integration
+ */
+class ClientTest extends \PHPUnit_Framework_TestCase
+{
+    const PORT = 7777;
+
+    const PHP_LOGO_PNG_BASE64 = <<<'EOF'
+iVBORw0KGgoAAAANSUhEUgAAAHkAAABACAYAAAA+j9gsAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAD4BJREFUeNrsnXtwXFUdx8/dBGihmE21QCrQDY6oZZykon/gY5qizjgM2KQMfzFAOioOA5KEh+j4R9oZH7zT6MAMKrNphZFSQreKHRgZmspLHSCJ2Co6tBtJk7Zps7tJs5t95F5/33PvWU4293F29ybdlPzaM3df2XPv+Zzf4/zOuWc1tkjl+T0HQ3SQC6SBSlD6WKN4rusGm9F1ps/o5mPriOf8dd0YoNfi0nt4ntB1PT4zYwzQkf3kR9/sW4xtpS0CmE0SyPUFUJXFMIxZcM0jAZ4xrKMudQT7963HBF0n6EaUjkP0vI9K9OEHWqJLkNW1s8mC2WgVTwGAqWTafJzTWTKZmQuZ/k1MpAi2+eys6mpWfVaAPzcILu8EVKoCAaYFtPxrAXo8qyNwzZc7gSgzgN9Hx0Ecn3j8xr4lyHOhNrlpaJIgptM5DjCdzrJ0Jmce6bWFkOpqs0MErA4gXIBuAmY53gFmOPCcdaTXCbq+n16PPLXjewMfGcgEttECeouTpk5MplhyKsPBTiXNYyULtwIW7Cx1vlwuJyDLR9L0mQiVPb27fhA54yBbGttMpc1OWwF1cmKaH2FSF7vAjGezOZZJZ9j0dIZlMhnuRiToMO0c+N4X7oksasgEt9XS2KZCHzoem2Ixq5zpAuDTqTR14FMslZyepeEI4Ogj26n0vLj33uiigExgMWRpt+CGCsEePZqoePM738BPTaJzT7CpU0nu1yXpAXCC3VeRkCW4bfJYFZo6dmJyQTW2tvZc1nb719iyZWc5fmZ6Osu6H3uVzit52oBnMll2YizGxk8muFZLAshb/YKtzQdcaO3Y2CQ7eiy+YNGvLN+4+nJetm3bxhKJxJz316xZw1pbW9kLew+w1944XBEaPj6eYCeOx1gqNe07bK1MwIDbKcOFOR49GuePT5fcfOMX2drPXcQ0zf7y2tvbWVdXF/v1k2+yQ4dPVpQ5P0Um/NjoCX6UBMFZR6k+u7qMYVBYDIEqBW7eXAfPZX19zp2/oaGBHysNMGTFinPZik9fWggbI5Omb13zUDeB3lLsdwaK/YPeyAFU0i8Aw9/2Dwyx4SPjFQEYUlf3MTYw4Jx7CIVCbHR0oqIDNMD+FMG+ZE0dO/tsHlvAWnYS6H4qjfMC+Zld/wg92/tuv2WeeYT87j+H2aFDxysGLuSy+o/z49DQkONnmpqa2MjRyoYsZOXKGnb5Z+vZqlUrxUsAvI9At/oK+elnBpoNw+Dai9TekSMxDrgSh0KrSYshTprc2NhoRf1JtlikqirAVl98AddsSavDBDrsC+QdT7/TSoB344tzOZ39+70RbporVerqasyw1MEnC8iV6I9VTDi0uqbmfPFSq2W+gyUHXuEdb3WR5rab5jnD3i/BNMN8ChNaqsTiKa55KmBWX+Tuj0XQdQVF307nhTH0CPls+O0UPbaT5TQG/8qX68u6LpV67LQ6dNknaYgaYyPDx2TzvYGCsnhRkH8b/rsF2GDj1MCInkvxvRjOuCUlipWD/zrKx7ZOwBF0vfSSM2ShyaqAAOC1Nw+zt9/5YNbrN1zfwIdpfgnqebv/A6pnWAn4qlW1HPgHQ6OeoG3N9RO/+StMdDtmV2LxJPfBpQCGfwTgrVu38jFrKaW2tpZt2LCBdXR0sEgkwhv21u9cxQsyW3ZB1+DgoOM54btU6tu8eTPr6elhy5fr7IZNDey+e76e9/fCLcAllHpdKKinpaUlX8+111xB9VzNrYxqUAY/XVVVJYMOekLu2fFGM8VWYQRYiYkU9bD4vPlHFYnH4/zvkb1CgwACHgMoUpdyw3sFXcXUh4YHaNSHDqaxdL5jwVTXBpeXVY9oF3RcUQ+O09NT7Cayfld+4RJlP42gTIq8w66Qf/X4a6FTSSMMDcaE/NhYecMM+MdyG90OAhodWoAGkTUaSZByO5WdiA4GqwStrrM6k5vFKEXQserr63l7oR5V0NBojKctaSZtbneErOtGmFxwkGewjk0UzpCUlJSIRqMcjN8CkHLDqyRByq0PEGBBhDmdj7rQVujAaLfrrlk7xyW5gUaxpEtOmOQDr0e799NYmDVBi0+OT7FcbsaXxEQk8qprEBQMBm0vVKUBRcNjskFE8W71lSt79uzhda1d6w4ZGTUUp3NWAQ3TvW/fPvbVq+rZH/ceULOcF1/I06CY3QJohCCzNJnYdgEwwvpUKuNbUsLNpO3evZtfSGHp7+/nS2pw3LLFPVWLoA5yHQUtXvXFYjH+vU4F5yOibzsRUL38MTqC3XWh8GCWziMcDjt2BNEZUIfoUOpJkwvziT3S5ua8Jj/4yD5E0yERbPkhKv4RF4mhkN1wCMHN2rWfYZ2dnWz9+vXchNkJzBoaQ8Bxqg91wWo41YdO2dzczD+3bt06Rw0rBG4nOF8oi9M0Jsw9OgLqQ124BifLgeuHyVbN0NXUrODBmDWxgRR0pNrUYqMNgDOZGZbNzvgCuc4j0kX+GPJ2//CcMagQmKkbrm/knwVEp++SIXulM1+nhj9AY207QRDnpsnye24WA59DkuPlV/5j+z5eB2hE0W1tbTyQdNJmDpksRzFp2E9csFJAboRvDvz8gZdJgw2ek55KZphfAv+Inu8UdKnmkEUHQK93EjEZ4Rbkifq8JiactEpYAy9Nli2Gm6CjIZPn1qlKFWizleOG3BIwdKNZ+KRMxr9VHKvr1NKLXo2BhlAVFRPq1qlWW6MBr3NWyY2rTGXO5ySJlN9uDuiGsV7XTVPtl8CHYGizf/9+V5Om0hAwVV4ahuU8qia03HP26kyqFkMOTudDzjs/P/QKBUiBYa5ZNucfZJUkCG/0IhpCxYyqBF3lnLOII8q1GKqdStQ3rTh5MStwXX5O/nE1metGQzPHUH6JatA1OppQ8u1eUbpX44tO4GY5vM5Z9sduFgOfG1GwUOK6VFzaSAmrWCSfzGCuuT/O+bi6QwRdTtqXN2keJ4/ejgkJ5HedRARkbkGe6ARulgMWQ+Wc3cDAWohhoZdcue7ifJ7crfP6Me8dELd0Mv8U2begC2k9SHd3t+NnNm7cqKwRbiYUkykqvlZlmOYVLIq5bHRep46JzotOc9BhuFc0ZHGLph+CJIaXr1FZSIfxsdBiN1+LpALEK2By61Aqs0rwtV7DNBU3BMCYixYTLU6C8bM5hBwum0k1mesBpmPtlj+qXFenFsAgCVLon9DYeIxUnmh05HCdBIkCVRP6ussiepVZJZXIutCHwt2I0YGY2Kiz3AIyeG5aLNooVULQBbHy1/nAK2oEtEanheil+GO3aFg0FnwSilNC4q6OrXzywc0XCy1WMaFu/tgrCBLRuWpHuP+n1zqmRXFN0GAnwKgHeW1E1C/86UDJHFKptATZMPZTafbLXHtN3OPixKRC4ev4GwB2Gy6JxhQNEYul+KoKp79RMaGqKzy9ovzt27c7pidVZtYAGJMYOP7u6bdK1mLI1GQ+/ogSZBahwKuLO2jSZt0odw65xrUhAMNrZskLsGiIXz72F3bTjV+ixvtbWcMQr3NWCbog5VyXAIy63PLrqpJITIqHkcD9P7suSiYbG53wvTLKDbr8WBbjZqIF4F3PD3ItRn1eQd5CBF3lCM5RAIYfVp0/dgZ8SvbJ2/l8MmlvNw+8qJTjm+drWQwaAXO9KMuWncc1GBMXKkGeV/pU5ZxFIsTvzovOCu3HvDnOE7NTu3rLr+PE8fy6+IEX9947YM4n/+LbPT/88R8QqoYAuVSDrZLFKcYso2AcLBIeGDPu6h3M+yqvIE/4Y6w4LdUfi+jcr86L75KvC9+PcbVfd1hCi6U7Innwk1/+Q5rcoetsdyBg3s9aCmivBsNFifGfG9zCJUFiztmpEXAbqhMgr6SLWBPu9R1enRfm1ktrC6cVYWH+/Mqg43x6sYK1edaCex7vkRZHZkF+6P6NkXvvi/TpLNBUaqTtdcsoLtIrVTcem2EHDh7m2uq0ikMINBvafOmazzt+BkGMW9CF70DndPsOaJqb38Y1oXjdCYHOiqwbPofrKid6thMAlnxxPtMy6w4K0ubNhq73U5wd5PtVleCTd+50D2CEafLloqixyv0ufMcOGq64CVaMYN2119gfAdPpuscKOxWgCMDwxfm0pvzBhx9siRLoFt3ca7Ikf+x2yygaYzHdTSi7IT9y8fMJ2Lpdhg+ZCPA2+f05d1A88mBLHzQaoA1dL6ohVLJGi+1uQj8XQMyHIMgaGT6eDxuozMkD294LRaB7CPI27DLHQSskSFRvGa30O/zndF4fF0DMhwa//9//iZ2DcILqN7xBHn1oUweNn7eJ3WO9QHvdMlrMsphKEj8XQPgpuHVVMtGOgF0hC9CGTqbb2kHOzXx73aKiuiymEv2x22ICMYYeWSALBQ7RQ0fkoZIr4DnRtS3ohzf1dNzTG9d0PcwMLahZO8UyKTMm38wteratSVtkplq4oWj0PcfrEinPhYg14H+hvdIwCVs1bvb6O+UBMYFGl90d0LRGLRDgoHEUwYnXDniQStocTVUwfPLaKQGA/RoWOmkvtnsaG8unK+PWMKlH5e+Lznp03N27RdO0TkxmYNZKszYBlyfI3RpjsQkmMOo8ls4Wsx1EKcEVAEvayyNoeRzsO2RI+93PNRLesGYtNpBhL4l/prlgZz5ob0mbtZVFhWC301d0EuQgAHPgS7D9hssTHKyMbRfLptF213NBDRuoaqxNA2yh2VUBDnxJ1M1yRW6gOgt2x64gqXK7ht1yOWyW1+wl7bYXvhUygQXgit4KuVDuBGzSbA2bmmtayNzpRgJOGu7XosHFChZzvrGTiUKt5UMiVsmbmtsCb3+2lZmwm3hFNsA/CiYdKyfhYx3Aws8urp8nsJM72naGCG8zYwZMecjk/WHVVRbsMwU6tBVQsWJS2sNDlrgVTO0RE/vzKQtuN2+/85k5PxlUaL75D3BZwKss+JUqSFRAO/F7Eqlkmj+2gbrgYE8rZFluu+P3pOGsyWCG/Y9/GR8exC+vYfc5flxgzRdDGsDEz/8AJsxwQcBUKPCtmKOMFJO8OKMgF8r3b3sKkAm69TN+2OZCAm5ID/g9XPypwX29ufWgudq0urrKes/8nPkxgy1bdg6z/or/SFc2mzV/xs+6HwySTmdYJp2dpaWKEregYrVfn9/B0xkD2U6+e+sOaHqImTfLrycUOIZM1hJwC3oemPXbi/y5PnsrJ136bUa8pxu69BklmANWwDRkgR1wmwVaglyi3Nz6JLQ+ZG5NxQsgNdAhmIfJN7wxgoWg9fxzPQ+c/g9YAIXgeUKCyipJO4uR/wswAOIwB/5IgxvbAAAAAElFTkSuQmCC
+EOF;
+
+    private static $client;
+
+    private static function getAuthenticationCode(Client $client)
+    {
+        // TODO: Figure out why the callback URL (passed as the second
+        // parameter) does not seem to matter here, but passing a non-empty
+        // string causes the authentication process to not be initiated.
+        $url = $client->getLoginUrl(array('wl.skydrive_update'), '');
+
+        echo "Integration test suite started.\n\nPlease sign into your OneDrive account from this page and grant to the app all\nthe privileges requested:\n\n\t$url\n\nThis process will then resume, do not interrupt it.\n";
+
+        $server = @socket_create_listen(self::PORT, 1);
+
+        if (false === $server) {
+            $message = socket_strerror(socket_last_error());
+            throw new \Exception($message);
+        }
+
+        $socketRemote = @socket_accept($server);
+
+        if (false === $socketRemote) {
+            $message = socket_strerror(socket_last_error());
+            socket_close($server);
+            throw new \Exception($message);
+        }
+
+        $buffer = @socket_read($socketRemote, 4096, PHP_BINARY_READ);
+
+        if (false === $buffer) {
+            $message = socket_strerror(socket_last_error());
+            socket_close($socketRemote);
+            socket_close($server);
+            throw new \Exception($message);
+        }
+
+        $size = @socket_write($socketRemote, implode("\r\n", array(
+            'HTTP/1.1 200 OK',
+            'Content-Type: text/html; charset=utf-8',
+            '',
+            '<!DOCTYPE html><h1>Thank you</h1><p>The integration test suite started running. You can close this window.</p>',
+        )));
+
+        if (false === $size) {
+            $message = socket_strerror(socket_last_error());
+            socket_close($socketRemote);
+            socket_close($server);
+            throw new \Exception($message);
+        }
+
+        socket_close($socketRemote);
+        socket_close($server);
+        list($headers, $body) = explode("\r\n\r\n", $buffer);
+
+        $headers = explode("\r\n", $headers);
+        $request = $headers[0];
+
+        if (1 !== preg_match('/^GET\s+(.+)\s+HTTP\/1\.1$/', $request, $matches)) {
+            throw new \Exception('Unsupported HTTP request format');
+        }
+
+        $url        = $matches[1];
+        $components = parse_url($url);
+        $query      = $components['query'];
+        $params     = explode('&', $query);
+        $query      = array();
+
+        array_map(function ($param) use (&$query) {
+            list($key, $value) = explode('=', $param);
+            $query[$key] = $value;
+        }, $params);
+
+        if (!array_key_exists('code', $query)) {
+            throw new \Exception('Code is missing from the request. Did you log in successfully and granted all the privileges requested?');
+        }
+
+        return $query['code'];
+    }
+
+    public static function setUpBeforeClass()
+    {
+        parent::setUpBeforeClass();
+        $config = sprintf('%s/config.php', __DIR__);
+
+        if (!file_exists($config)) {
+            throw new \Exception("Configuration file not found. Please create a $config file from the sample provided.");
+        }
+
+        $config = require $config;
+        $url    = sprintf('http://localhost:%u/', self::PORT);
+
+        $client = new Client(array(
+            'client_id' => $config['CLIENT_ID'],
+        ), $url);
+
+        try {
+            $code = self::getAuthenticationCode($client);
+        } catch (\Exception $e) {
+            die($e->getMessage() . "\n");
+        }
+
+        $client->obtainAccessToken($config['SECRET'], $code);
+        self::$client = $client;
+    }
+
+    public function testCreateFolder()
+    {
+        $folder1 = self::$client->createFolder('Test folder #1');
+        $this->assertNotNull($folder1);
+
+        $folder2 = self::$client->createFolder('Test folder #2');
+        $this->assertNotNull($folder2);
+
+        return array($folder1, $folder2);
+    }
+
+    /**
+     * @depends testCreateFolder
+     */
+    public function testCreateFile($arguments)
+    {
+        $folder1 = $arguments[0];
+        $folder2 = $arguments[1];
+
+        // Test with a text file.
+        $file1 = self::$client->createFile('Test file #1.txt', $folder1->getId(), 'Test content');
+        $this->assertNotNull($file1);
+
+        $file1 = self::$client->fetchObject($file1->getId());
+        $this->assertNotNull($file1);
+
+        $actual = $file1->fetchContent();
+        $this->assertEquals('Test content', $actual);
+
+        // Test with a binary file.
+        $file2 = self::$client->createFile('Test file #2.png', $folder2->getId(), base64_decode(self::PHP_LOGO_PNG_BASE64));
+        $this->assertNotNull($file2);
+
+        return array($folder1, $folder2, $file1);
+    }
+
+    /**
+     * @depends testCreateFile
+     */
+    public function testMoveObject($arguments)
+    {
+        $folder1 = $arguments[0];
+        $folder2 = $arguments[1];
+        $file1   = $arguments[2];
+
+        self::$client->moveObject($file1->getId(), $folder2->getId());
+
+        return array($folder1, $folder2, $file1);
+    }
+
+    /**
+     * @depends testMoveObject
+     */
+    public function testCopyObject($arguments)
+    {
+        $folder1 = $arguments[0];
+        $folder2 = $arguments[1];
+        $file1   = $arguments[2];
+
+        self::$client->copyFile($file1->getId(), $folder1->getId());
+
+        return array($folder1, $folder2);
+    }
+
+    /**
+     * @depends testCopyObject
+     */
+    public function testDeleteObject($arguments)
+    {
+        $folder1 = $arguments[0];
+        $folder2 = $arguments[1];
+
+        self::$client->deleteObject($folder1->getId());
+
+        self::$client->deleteObject($folder2->getId());
+
+        $this->assertTrue(true);
+    }
+}
