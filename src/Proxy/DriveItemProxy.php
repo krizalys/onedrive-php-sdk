61af2f280b43fc5cd3d583016e0b3a791ab3797f
ardabeyazoglu <arda@beyazoglu.com>
2018-12-02 18:29:55 +0100

    add status code 200

--- src/Proxy/DriveItemProxy.php
+++ src/Proxy/DriveItemProxy.php
@@ -185,7 +185,7 @@ class DriveItemProxy extends BaseItemProxy
 
         $status = $response->getStatus();
 
-        if ($status != 201) {
+        if ($status != 200 && $status != 201) {
             throw new \Exception("Unexpected status code produced by 'POST $endpoint': $status");
         }
 
@@ -287,7 +287,7 @@ class DriveItemProxy extends BaseItemProxy
 
         $status = $response->getStatus();
 
-        if ($status != 201) {
+        if ($status != 200 && $status != 201) {
             throw new \Exception("Unexpected status code produced by 'PUT $endpoint': $status");
         }
 
