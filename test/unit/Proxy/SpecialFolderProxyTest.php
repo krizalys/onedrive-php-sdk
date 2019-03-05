17fd7f3f6f4dd4c2bd52fc697c1671e3667df88b
Christophe Vidal <kriss@krizalys.com>
2019-03-05 23:22:00 +0700

    Removed unused variable

--- test/unit/Proxy/SpecialFolderProxyTest.php
+++ test/unit/Proxy/SpecialFolderProxyTest.php
@@ -11,7 +11,6 @@ class SpecialFolderProxyTest extends \PHPUnit_Framework_TestCase
     public function testNameShouldReturnExpectedValue()
     {
         $graph         = $this->createMock(Graph::class);
-        $systemFacet   = $this->createMock(SpecialFolder::class);
         $specialFolder = $this->createMock(SpecialFolder::class);
         $specialFolder->method('getName')->willReturn('Name');
         $sut = new SpecialFolderProxy($graph, $specialFolder);
