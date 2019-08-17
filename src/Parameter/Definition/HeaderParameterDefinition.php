680c265a2193ba008a17fafe254e523ca38ff692
Christophe Vidal <kriss@krizalys.com>
2019-08-17 10:40:36 +0700

    Added Krizalys\Onedrive\Parameter\Definition\HeaderParameterDefinition

---
+++ src/Parameter/Definition/HeaderParameterDefinition.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * This file is part of Krizalys' OneDrive SDK for PHP.
+ *
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
+ *
+ * @author    Christophe Vidal
+ * @copyright 2008-2019 Christophe Vidal (http://www.krizalys.com)
+ * @license   https://opensource.org/licenses/BSD-3-Clause 3-Clause BSD License
+ * @link      https://github.com/krizalys/onedrive-php-sdk
+ */
+
+namespace Krizalys\Onedrive\Parameter\Definition;
+
+/**
+ * An implementation for an HTTP header parameter definition.
+ *
+ * @since 2.3.0
+ */
+class HeaderParameterDefinition extends AbstractParameterDefinition
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function serializeKey()
+    {
+        return $this->name;
+    }
+}
