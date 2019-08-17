4d904bd360b207b6e748142e9769abad43449c53
Christophe Vidal <kriss@krizalys.com>
2019-08-17 10:41:03 +0700

    Added Krizalys\Onedrive\Parameter\Definition\QueryStringParameterDefinition

---
+++ src/Parameter/Definition/QueryStringParameterDefinition.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * This file is part of Krizalys' OneDrive SDK for PHP.
+ *
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
+ *
+ * @author    Christophe Vidal
+ * @copyright 2008-2019 Christophe Vidal (http://www.krizalys.com)
+ * @license   https://opensource.org/licenses/BSD-3-Clause 3-Clause BSD License
+ * @link      https://github.com/krizalys/onedrive-php-sdk
+ */
+
+namespace Krizalys\Onedrive\Parameter\Definition;
+
+/**
+ * An implementation for a query string parameter definition.
+ *
+ * These parameters always have their name prefixed by '$', as recommended by
+ * the OneDrive documentation.
+ *
+ * @since 2.3.0
+ */
+class QueryStringParameterDefinition extends AbstractParameterDefinition
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function serializeKey()
+    {
+        return "\${$this->name}";
+    }
+}
