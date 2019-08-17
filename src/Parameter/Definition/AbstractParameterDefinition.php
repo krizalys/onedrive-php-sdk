82dfa25a0f56991e6dd6ddaa0268f830ee848188
Christophe Vidal <kriss@krizalys.com>
2019-08-17 11:29:31 +0700

    Added Krizalys\Onedrive\Parameter\Definition\AbstractParameterDefinition

---
+++ src/Parameter/Definition/AbstractParameterDefinition.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * This file is part of Krizalys' OneDrive SDK for PHP.
+ *
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
+ *
+ * @author    Christophe Vidal
+ * @copyright 2008-2019 Christophe Vidal (http://www.krizalys.com)
+ * @license   https://opensource.org/licenses/BSD-3-Clause 3-Clause BSD License
+ * @link      https://github.com/krizalys/onedrive-php-sdk
+ */
+
+namespace Krizalys\Onedrive\Parameter\Definition;
+
+use Krizalys\Onedrive\Serializer\SerializerInterface;
+
+/**
+ * An abstract implementation for a parameter definition.
+ *
+ * @since 2.3.0
+ */
+abstract class AbstractParameterDefinition implements ParameterDefinitionInterface
+{
+    /**
+     * Constructor.
+     *
+     * @param string $name
+     *        The header parameter name.
+     * @param SerializerInterface $serializer
+     *        The serializer.
+     *
+     * @since 2.3.0
+     */
+    public function __construct($name, SerializerInterface $serializer)
+    {
+        $this->name       = $name;
+        $this->serializer = $serializer;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    abstract public function serializeKey();
+
+    /**
+     * {@inheritdoc}
+     */
+    public function serializeValue($value)
+    {
+        return $this->serializer->serialize($value);
+    }
+}
