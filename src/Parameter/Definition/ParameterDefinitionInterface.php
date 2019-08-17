d8f448163b3eba658dfcafa765c83b80eb54de7a
Christophe Vidal <kriss@krizalys.com>
2019-08-17 10:40:22 +0700

    Added Krizalys\Onedrive\Parameter\Definition\ParameterDefinitionInterface

---
+++ src/Parameter/Definition/ParameterDefinitionInterface.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * This file is part of Krizalys' OneDrive SDK for PHP.
+ *
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
+ *
+ * @author    Christophe Vidal
+ * @copyright 2008-2019 Christophe Vidal (http://www.krizalys.com)
+ * @license   https://opensource.org/licenses/BSD-3-Clause 3-Clause BSD License
+ * @link      https://github.com/krizalys/onedrive-php-sdk
+ */
+
+namespace Krizalys\Onedrive\Parameter\Definition;
+
+/**
+ * An interface defining the contract for a parameter definition.
+ *
+ * @since 2.3.0
+ */
+interface ParameterDefinitionInterface
+{
+    /**
+     * Serializes this instance' key into a string.
+     *
+     * @return string
+     *         The serialized key.
+     *
+     * @since 2.3.0
+     */
+    public function serializeKey();
+
+    /**
+     * Serializes a given value into a string using this instance.
+     *
+     * @param mixed $value
+     *        The value to serialize.
+     *
+     * @return string
+     *         The serialized value.
+     *
+     * @since 2.3.0
+     */
+    public function serializeValue($value);
+}
